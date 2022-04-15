4a7cc4acdc368f8893cfc847b098bf9a7fb4e41a
Eugene Kolenko <kolenko.evg@gmail.com>
2022-04-15 18:43:22 +0700

    Added TASK_1

--- CS_Homework_2/TASK_1/Program_10.cs
+++ CS_Homework_2/TASK_1/Program_10.cs
@@ -1,2 +1,30 @@
-﻿Console.Write("Please input a three-digit number: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
+﻿/*Задача 10: Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.*/
+
+Console.Write("Please input a three-digit number: ");
+
+try 
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if ( number < 100 || number > 999) 
+    {
+        
+        Console.WriteLine("Error! You inputed not a three-digit number!");
+
+    }
+    else
+    {
+
+        Console.WriteLine("The second digit of number: " + number / 10 % 10); 
+
+    }
+}
+catch (System.FormatException)
+{
+
+    Console.WriteLine("Error! You inputed not an integer number!");
+
+}
+
