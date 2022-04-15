759507dd6b7df0f1fe0230289b1fa1ec609d39e3
Eugene Kolenko <kolenko.evg@gmail.com>
2022-04-15 19:34:34 +0700

    Added TASK_2 & fixed TASK_1

--- CS_Homework_2/TASK_1/Program_10.cs
+++ CS_Homework_2/TASK_1/Program_10.cs
@@ -2,29 +2,41 @@
 вход трёхзначное число и на выходе показывает вторую
 цифру этого числа.*/
 
-Console.Write("Please input a three-digit number: ");
-
-try 
+for (;;)
 {
-    int number = Convert.ToInt32(Console.ReadLine());
 
-    if ( number < 100 || number > 999) 
+    Console.Write("Please input a three-digit number: ");
+
+    try
     {
-        
-        Console.WriteLine("Error! You inputed not a three-digit number!");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        number = Math.Abs(number);
+
+        if (number < 100 || number > 999)
+        {
+
+            Console.WriteLine("Error! You inputed not a three-digit number!");
+
+        }
+        else
+        {
+
+            Console.WriteLine("The second digit of number: " + number / 10 % 10);
+            break;
+
+        }
+    }
+    catch (System.FormatException)
+    {
+
+        Console.WriteLine("Error! You inputed not an integer number!");
 
     }
-    else
+    catch (System.OverflowException)
     {
 
-        Console.WriteLine("The second digit of number: " + number / 10 % 10); 
+        Console.WriteLine("Error! You inputed too big number for int type!");
 
     }
 }
-catch (System.FormatException)
-{
-
-    Console.WriteLine("Error! You inputed not an integer number!");
-
-}
-
---
+++ CS_Homework_2/TASK_2/Program_13.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 13: Напишите программу, которая выводит
+третью цифру заданного числа или сообщает, что третьей
+цифры нет.*/
+
+for (; ; )
+{
+
+    Console.Write("Please input integer number: ");
+
+    try
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        number = Math.Abs(number);
+
+        if (number < 100)
+        {
+
+            Console.WriteLine("Inputted number has no third digit!");
+
+        }
+        else
+        {
+
+            while (number > 999) number /= 10;            
+            
+            Console.WriteLine("The third digit of number: " + number % 10);
+            break;
+
+        }
+    }
+    catch (System.FormatException)
+    {
+
+        Console.WriteLine("Error! You inputed not integer number!");
+
+    }
+    catch (System.OverflowException)
+    {
+
+        Console.WriteLine("Error! You inputed too big number for int type!");
+
+    }
+
+}
