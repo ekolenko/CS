0a9b4caf08e15ed177741f1977ba52827e94d5b3
Евгений Коленко <ek0lenk0@fm.local>
2022-04-18 15:08:16 +0700

    Added other variant

--- CS_Lesson_3_P/TASK_2/Program_18.cs
+++ CS_Lesson_3_P/TASK_2/Program_18.cs
@@ -1,18 +1,35 @@
-﻿Console.Write("Please input a quadrant number: ");
-int qNumber = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Write("Please input a quadrant number: ");
+// int qNumber = Convert.ToInt32(Console.ReadLine());
 
 
- string getRange(int qNumber)
+//  string getRange(int qNumber)
+//  {
+
+//      if (qNumber == 1) return "X > 0 , Y > 0";    
+//      if (qNumber == 2) return "X > 0 , Y < 0";
+//      if (qNumber == 3) return "X < 0 , Y < 0";
+//      if (qNumber == 4) return "X < 0 , Y > 0";
+
+
+//      return "-1";
+
+//  }
+
+// Console.WriteLine(getRange(qNumber));
+
+ Console.Write("Please input a quadrant number: ");
+ 
+ string getRange(string anyString2)
  {
 
-     if (qNumber == 1) return "X > 0 , Y > 0";    
-     if (qNumber == 2) return "X > 0 , Y < 0";
-     if (qNumber == 3) return "X < 0 , Y < 0";
-     if (qNumber == 4) return "X < 0 , Y > 0";
+     if (anyString2 == "1") return "X > 0 , Y > 0";    
+     if (anyString2 == "2") return "X > 0 , Y < 0";
+     if (anyString2 == "3") return "X < 0 , Y < 0";
+     if (anyString2 == "4") return "X < 0 , Y > 0";
 
 
-     return "-1";
+     return "You inputed wrong quadrant!";
 
  }
 
- Console.WriteLine(getRange(qNumber));
+ Console.WriteLine(getRange(Console.ReadLine()));
