2fe9a22e3342f757c16f0b32b205512d3abb498b
Евгений Коленко <ek0lenk0@fm.local>
2022-04-18 14:40:22 +0700

    Added TASK_1

---
+++ CS_Lesson_3_P/TASK_1/Program_17.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Please input a number X: ");
+int numberX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input a number Y: ");
+int numberY = Convert.ToInt32(Console.ReadLine());
+
+int Part(int x, int y)
+{
+
+    if (x > 0 && y > 0) return 1;
+    if (x > 0 && y < 0) return 2;
+    if (x < 0 && y < 0) return 3; 
+    if (x < 0 && y > 0) return 4;
+    
+    return -1;
+
+}
+
+Console.WriteLine(Part(numberX,numberY));
