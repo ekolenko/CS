040f8f597c535a0dee298add4269d83c898e85c2
Eugene Kolenko <kolenko.evg@gmail.com>
2022-04-18 20:36:50 +0700

    Added TASK_3

---
+++ CS_Homework_3/TASK_3/Program_23.cs
@@ -0,0 +1,52 @@
+﻿
+/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N*/
+
+for (;;)
+{
+
+    Console.Write("Please input integer number: ");
+
+    try
+    {
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        printCube(number); 
+        break;       
+
+    }
+
+    catch (System.FormatException)
+    {
+
+        Console.WriteLine("Error! You inputed not integer number!");
+
+    }
+
+    catch (System.OverflowException)
+    {
+
+        Console.WriteLine("Error! You inputed too big number for int type!");
+
+    }
+
+}
+
+void printCube(int anyNumber)
+{
+
+    for (int i = 1; i <= anyNumber; i++)
+    {
+
+        Console.WriteLine(i + " -> " + Cube(i));
+
+    }
+
+}
+
+double Cube(int n)
+{
+
+    return Math.Pow(n, 3);
+
+}
