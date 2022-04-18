367bbd9cf512a3cde36677fcc6a34171f8470643
Евгений Коленко <ek0lenk0@fm.local>
2022-04-18 15:00:40 +0700

    Added TASK_2

---
+++ CS_Lesson_3_P/TASK_2/Program_18.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Please input a quadrant number: ");
+int qNumber = Convert.ToInt32(Console.ReadLine());
+
+
+ string getRange(int qNumber)
+ {
+
+     if (qNumber == 1) return "X > 0 , Y > 0";    
+     if (qNumber == 2) return "X > 0 , Y < 0";
+     if (qNumber == 3) return "X < 0 , Y < 0";
+     if (qNumber == 4) return "X < 0 , Y > 0";
+
+
+     return "-1";
+
+ }
+
+ Console.WriteLine(getRange(qNumber));
