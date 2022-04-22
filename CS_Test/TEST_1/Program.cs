c8096ca3268348b39a852f465b3d313c94d8d2e5
Eugene Kolenko <kolenko.evg@gmail.com>
2022-04-22 23:31:07 +0700

    Added My TEST_1

---
+++ CS_Test/TEST_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+int sum = 0;
+int [] arr = new int [10000000];
+
+int count = arr.Length;
+
+for (int i = 0; i < count; i++)
+{
+
+    arr[i] = i;
+
+}
+
+
+for (int i = 0; i < count; i++)
+{
+
+    sum += arr[i];
+
+}
+
+
+
+stopwatch.Stop();
+Console.WriteLine(sum);
+Console.WriteLine(stopwatch.ElapsedTicks);
