ab07ec4ce9a6b011df8cf0e1984f5d266336038a
Eugene Kolenko <kolenko.evg@gmail.com>
2022-04-22 20:52:07 +0700

    Added TASK_1

---
+++ CS_Homework_4/TASK_1/Program_25.cs
@@ -0,0 +1,49 @@
+﻿/*Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B*/
+
+int numberA = InputIntNumber("A");
+int numberB;
+
+while ((numberB = InputIntNumber("B")) < 0)
+{
+
+    Console.WriteLine("The power must be positive! Try again");
+
+}
+
+Console.WriteLine($"{numberA} to the power of {numberB} = {MyPow(numberA,numberB)}");
+
+
+double MyPow (int a, int b)
+{   
+    double result = 1;
+
+    for (; b > 0; b--)
+    {
+
+        result *= a;
+
+    }
+
+    return result;
+
+}
+
+
+int InputIntNumber(string numberName)
+{   
+    Console.Write($"Input integer number {numberName}: ");
+
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write($"Input integer number {numberName}: ");
+        
+    }
+
+    return number;
+}
---
+++ CS_Lesson_4_P/TASK_1/Program_24.cs
@@ -0,0 +1,43 @@
+﻿/* Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А. */
+
+int InputNumber(string Text)
+{
+    for (;;)
+    {
+        try
+        {
+
+            Console.Write(Text);
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+
+        }
+
+        catch (System.FormatException)
+        {
+
+            Console.WriteLine("Error! You inputed not integer number!");
+
+        }
+        catch (System.OverflowException)
+        {
+
+            Console.WriteLine("Error! You inputed too big number for int type!");
+
+        }
+    }
+
+}
+
+int numberA = InputNumber("Input number A: ");
+int result = 0;
+for (int i = 1; i <= numberA;  i++ )
+{
+
+    result +=i;
+
+}
+
+Console.WriteLine($"Summary = {result}");
