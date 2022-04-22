14fab5ae23978f2883eb660f782c71c8c609bcd0
Eugene Kolenko <kolenko.evg@gmail.com>
2022-04-22 21:19:15 +0700

    Added TASK_2

---
+++ CS_Homework_4/TASK_2/Program_27.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает на
+вход число и выдаёт сумму цифр в числе.
+*/
+
+int number = InputIntNumber("");
+int tempNumber = Math.Abs(number);
+int sum = 0;
+
+for (; tempNumber > 0; tempNumber /=10) 
+{   
+    sum += tempNumber % 10;
+}
+
+Console.WriteLine($"Digits sum of number {number} = {sum}");
+
+
+int InputIntNumber(string numberName)
+{   
+    Console.Write($"Input integer number {numberName}: ");
+
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write($"Input integer number {numberName}: ");
+        
+    }
+
+    return number;
+}
