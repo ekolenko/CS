894c3e46720dadfc060352bb19400c1bc10b06c9
Евгений Коленко <ek0lenk0@fm.local>
2022-04-25 17:22:54 +0700

    Added TASK_3

---
+++ CS_Homework_5/TASK_3/Program_38.cs
@@ -0,0 +1,80 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным значением элементов массива. 
+Длина массива задается с терминала.*/
+
+int arrLength = InputIntNumber("Input array size: ");
+double[] arr = new double [arrLength];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine("Difference between MAX and MIN value = " + FindDifferenceMaxMin(arr));
+
+
+int InputIntNumber(string messageText)
+{   
+    Console.Write(messageText);
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(messageText);
+        
+    }
+
+    return number;
+}
+
+double InputDoubleNumber(string messageText)
+{   
+    Console.Write(messageText);
+    double number;
+    
+    while (!double.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(messageText);
+        
+    }
+
+    return number;
+}
+
+void FillArray( double[] arrayIn)
+{
+    for (int i = 0; i < arrayIn.Length; i++)
+        arrayIn[i] = InputDoubleNumber($"Input array element [{i}]: ");
+}
+
+void PrintArray(double[] arrayIn) 
+{    
+    string someChars;
+    Console.Write("[");
+
+    for (int i = 0; i < arrayIn.Length; i++)
+    {   
+        someChars = (i == 0)?"":" ";
+        Console.Write(someChars + arrayIn[i]);
+
+    }
+
+    Console.WriteLine("]");
+}
+
+double FindDifferenceMaxMin(double[] arrayIn)
+{   
+    double max = arrayIn[0];
+    double min = arrayIn[0];
+
+    foreach (double element in arrayIn)
+    {
+        if (element > max) 
+            max = element;
+        else if (element < min)
+            min = element;
+    };
+
+    return Math.Abs(max - min);    
+
+}
