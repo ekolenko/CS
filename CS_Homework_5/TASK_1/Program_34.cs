a21c37d736653f82d2f76ba95fb8c90e023d741e
Евгений Коленко <ek0lenk0@fm.local>
2022-04-25 16:36:08 +0700

    Added TASK_1

---
+++ CS_Homework_5/TASK_1/Program_34.cs
@@ -0,0 +1,61 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве. 
+Длина массива задается с терминала.*/
+
+const int FROM = 100;
+const int TO = 1000;
+
+
+int arrLength = InputIntNumber("Input array size: ");
+int[] arr = new int [arrLength];
+RandomizeArray(arr, FROM, TO);
+PrintArray(arr);
+Console.WriteLine("Number of even elements = " + FindCountEvenElements(arr));
+
+
+int InputIntNumber(string messageText)
+{   
+    Console.Write(messageText);
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(messageText);
+        
+    }
+
+    return number;
+}
+
+void RandomizeArray(int[] arrayIn, int from, int to) 
+{
+    for (int i = 0; i < arrayIn.Length; i++)        
+        arrayIn[i] = new Random().Next(from,to);
+}
+
+void PrintArray(int[] arrayIn) 
+{    
+    string someChars;
+    Console.Write("[");
+
+    for (int i = 0; i < arrayIn.Length; i++)
+    {   
+
+        someChars = (i == 0)?"":", ";
+        Console.Write(someChars+arrayIn[i]);
+
+    }
+
+    Console.WriteLine("]");
+}
+
+int FindCountEvenElements(int[] arrayIn)
+{
+    int result = 0;
+    foreach (int element in arrayIn)
+        if (element % 2 == 0) result++;
+
+    return result;
+}
---
+++ CS_Test/MY_FUNC/Program.cs
@@ -0,0 +1,43 @@
+﻿void PrintArray(int[] arrayIn) 
+{    
+    string someChars;
+    Console.Write("[");
+
+    for (int i = 0; i < arrayIn.Length; i++)
+    {   
+
+        someChars = (i == 0)?"":", ";
+        Console.Write(someChars+arrayIn[i]);
+
+    }
+
+    Console.WriteLine("]");
+}
+
+void RandomizeArray(int[] arrayIn, int from, int to) 
+{
+    for (int i = 0; i < arrayIn.Length; i++)        
+        arrayIn[i] = new Random().Next(from,to);
+}
+
+void RandomizeArray(int[] arrayIn) 
+{
+    for (int i = 0; i < arrayIn.Length; i++)        
+        arrayIn[i] = new Random().Next();
+}
+
+int InputIntNumber(string numberName)
+{   
+    Console.Write($"Input integer number {numberName}: ");
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write($"Input integer number {numberName}: ");
+        
+    }
+
+    return number;
+}
