a5e237b77d201bef32225ef6589cc53b66fbc110
Евгений Коленко <ek0lenk0@fm.local>
2022-04-25 16:48:47 +0700

    Added TASK_2

---
+++ CS_Homework_5/TASK_2/Program_36.cs
@@ -0,0 +1,59 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях. 
+Длина массива задается с терминала.*/
+
+int arrLength = InputIntNumber("Input array size: ");
+int[] arr = new int [arrLength];
+
+RandomizeArray(arr, int.MinValue, int.MaxValue);
+PrintArray(arr);
+Console.WriteLine("Sum of odd index elements = " + FindSumOddIndexElements(arr));
+
+
+
+int InputIntNumber(string messageText)
+{   
+    Console.Write(messageText);
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(messageText);
+        
+    }
+
+    return number;
+}
+
+void RandomizeArray(int[] arrayIn, int from, int to) 
+{
+    for (int i = 0; i < arrayIn.Length; i++)        
+        arrayIn[i] = new Random().Next(from,to);
+}
+
+void PrintArray(int[] arrayIn) 
+{    
+    string someChars;
+    Console.Write("[");
+
+    for (int i = 0; i < arrayIn.Length; i++)
+    {   
+
+        someChars = (i == 0)?"":", ";
+        Console.Write(someChars+arrayIn[i]);
+
+    }
+
+    Console.WriteLine("]");
+}
+
+long FindSumOddIndexElements(int[] arrayIn)
+{
+    long result = 0;
+    for (int i = 1; i < arrayIn.Length; i += 2)
+        result += arrayIn[i];
+
+    return result;
+}
