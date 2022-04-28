4faa4b708055b628faa462831dcb70093cd7e88a
Евгений Коленко <ek0lenk0@fm.local>
2022-04-28 14:03:33 +0700

    Added my func

---
+++ CS_Test/TEST_2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int result = 2;
+int[] arr = new int[5];
+
+
+for (int i = 0; i < 5; i++)
+    arr[i] = new Random().Next(0,10);
+MyFunc(arr);
+
+/*static*/ void MyFunc (int[] arrayIn)
+{
+result = 1;
+foreach (int e in arrayIn)
+   Console.Write(e+" "+result);
+
+}
+
+Console.WriteLine("\n"+result);
