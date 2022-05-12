753e10a36b573f2da8e823ff37084130743075c8
Eugene Kolenko <kolenko.evg@gmail.com>
2022-05-12 14:01:07 +0600

    Added TASK_1 , TASK_2

---
+++ CS_Lesson_7_P/TASK_1/Program.cs
@@ -0,0 +1,27 @@
+﻿const int m = 4;
+const int n = 3;
+
+int [,] arr = new int [m, n];
+
+RandomizeArray(arr, -99, 100);
+
+PrintArray(arr);
+
+void RandomizeArray(int[,] arrayIn, int min, int max) 
+{
+    for (int i = 0; i < arrayIn.GetLength(0); i++)  
+        for (int j = 0; j < arrayIn.GetLength(1); j++)      
+            arrayIn[i,j] = new Random().Next(min,max);
+}
+
+void PrintArray(int[,] arrayIn)
+{
+    for (int i = 0; i < arrayIn.GetLength(0); i++)  
+    {
+        for (int j = 0; j < arrayIn.GetLength(1); j++) 
+        {
+            System.Console.Write(arrayIn[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
---
+++ CS_Lesson_7_P/TASK_2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задайте двумерный массив размера m на n, каждый элемент в массиве 
+находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран
+*/
+
+int m = InputIntNumber("M");
+int n = InputIntNumber("N");
+
+int [,] arr = new int [m,n];
+
+FillArray(arr);
+PrintArray(arr);
+
+int InputIntNumber(string numberName)
+{   
+    Console.Write($"Input integer number {numberName}: ");
+    int number;
+    
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write($"Input integer number {numberName}: ");
+        
+    }
+
+    return number;
+}
+
+void FillArray(int [,] arrayIn)
+{
+    for (int i = 0; i < arrayIn.GetLength(0); i++)  
+        for (int j = 0; j < arrayIn.GetLength(1); j++)      
+            arrayIn[i,j] = i + j;
+}
+
+void PrintArray(int [,] arrayIn)
+{
+    for (int i = 0; i < arrayIn.GetLength(0); i++)  
+    {
+        for (int j = 0; j < arrayIn.GetLength(1); j++) 
+        {
+            System.Console.Write(arrayIn[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
