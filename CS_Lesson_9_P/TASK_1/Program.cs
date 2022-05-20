4bf92160ac9ef1df5930187328b507a5c344dd72
Eugene Kolenko <kolenko.evg@gmail.com>
2022-05-20 14:07:59 +0600

    Added TASK_1, TASK_2

---
+++ CS_Lesson_9_P/TASK_1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+PrintNumber(InputNumber(""));
+
+void PrintNumber(uint n)
+{
+
+    if (n == 1)
+    {
+        Console.Write(n + " ");    
+    }
+    else 
+    {
+        PrintNumber(n - 1);
+        Console.Write(n + " ");
+    }
+}
+
+uint InputNumber(string message)
+{
+    Console.Write(message);
+    uint number;
+
+    while (!uint.TryParse(Console.ReadLine(), out number) & number < 0)
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(message);
+
+    }
+
+    return number;
+}
---
+++ CS_Lesson_9_P/TASK_2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 65: Дано натуральное число N. Выведите слово YES, 
+если число N является точной степенью двойки, или слово NO в противном случае. 
+Операцией возведения в степень пользоваться нельзя. N = 5 -> “NO" N = 4096 -> “YES"
+*/
+
+uint n = InputNumber(":");
+
+if (n < 2) System.Console.WriteLine("Number less 2");
+
+if (check(n))
+    System.Console.WriteLine("yes");
+else
+    System.Console.WriteLine("no");
+
+bool check(uint n)
+{
+
+    if (n == 2)
+        return true;
+
+    if (n % 2 > 0)
+        return false;
+    else
+        return check(n / 2);
+
+}
+
+uint InputNumber(string message)
+{
+    Console.Write(message);
+    uint number;
+
+    while (!uint.TryParse(Console.ReadLine(), out number) & number < 0)
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(message);
+
+    }
+
+    return number;
+}
