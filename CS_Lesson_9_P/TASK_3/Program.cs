07f54f26aa41d9d3ab26a3e01393a41b03a20d81
Eugene Kolenko <kolenko.evg@gmail.com>
2022-05-20 14:57:07 +0600

    Added TASK_3, TASK_4

---
+++ CS_Lesson_9_P/TASK_3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать 
+на вход число и возвращать сумму его цифр.*/
+
+uint n = InputNumber(":");
+//uint sum = 0;
+
+System.Console.WriteLine(sumM(n));
+
+uint sumM(uint n)
+{
+
+    if (n / 10 == 0)
+    {
+        return n % 10;
+    }
+    else
+    {
+        return n % 10 + sumM(n / 10);
+    }
+
+}
+
+uint InputNumber(string message)
+{
+    Console.Write(message);
+    uint number;
+
+    while (!uint.TryParse(Console.ReadLine(), out number) & number < 0)
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(message);
+
+    }
+
+    return number;
+}
---
+++ CS_Lesson_9_P/TASK_4/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.*/
+
+int a = InputNumber("-> ");
+int b = InputNumber("-> ");
+//uint sum = 0;
+
+System.Console.WriteLine(Pow(a,b));
+
+int Pow(int a, int b)
+{
+
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return a * Pow(a, --b);
+    }
+
+}
+
+int InputNumber(string message)
+{
+    Console.Write(message);
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number) & number < 0)
+    {
+
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write(message);
+
+    }
+
+    return number;
+}
