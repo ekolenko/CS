e46f3d941a6bd7bf7bf58b56cd04005a35ef4450
Eugene Kolenko <kolenko.evg@gmail.com>
2022-05-20 13:01:11 +0600

    Some test

---
+++ CS_Test/TEST_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+const int line = 10000;
+const int column = 10000;
+const int min = 0;
+const int max = 10;
+
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+
+int[,] array = new int[line, column];
+
+FillArray(array, min, max, line, column);
+
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+void FillArray(int[,] arr, int minValue, int maxValue, int line, int column) //Сам я здесь не смог справиться 
+{
+    int i = 0; 
+    int j = 0;
+    int a = 1; 
+    int b = 0; 
+    int directChanges = 0;
+    int count = column;
+
+    for (int k = 0; k < arr.Length; k++)
+    {
+        arr[i, j] = k + 1;
+        count--;
+        if (count == 0)
+        {
+            count = column * (directChanges % 2) + line * ((directChanges + 1) % 2) - (directChanges / 2 - 1) - 2;
+            int temp = a;
+            a = -b;
+            b = temp;
+            directChanges++;
+        }
+        i += b;
+        j += a;
+    }
+}
---
+++ CS_Test/TEST_5/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Задача 62: Заполните спирально массив 4 на 4.*/
+using System.Diagnostics;
+
+const uint m = 10000;
+const uint n = 10000;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+int step = 0;
+long arrSize = m * n;
+
+int[,] arr = new int[m, n];
+
+SpiralArray(arr);
+
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+
+void SpiralArray(int[,] arrayIn)
+{   
+    int startRow = 0;
+    int startColumn = 0;
+    int endRow = arrayIn.GetLength(0) - 1;
+    int endColumn = arrayIn.GetLength(1) - 1;
+        
+    while (step < arrSize)
+    {   
+        for (int i = 0; i < 4 && step < arrSize; i++)
+            FillLine(arrayIn, i, ref startRow, ref startColumn, ref endRow, ref endColumn);
+    }
+}
+
+void FillLine (int[,] arrayIn, int moveDirection, ref int startRow, ref int startColumn, ref int endRow, ref int endColumn)
+{
+    switch (moveDirection)
+    {
+        case 0: FillForward(arrayIn, startColumn, endColumn, true, startRow);
+                startRow++;
+                break;
+        case 1: FillForward(arrayIn, startRow, endRow, false, endColumn);
+                endColumn--;
+                break;
+        case 2: FillBack(arrayIn, endColumn, startColumn, true, endRow);
+                endRow--;
+                break;
+        case 3: FillBack(arrayIn, endRow, startRow, false, startColumn);
+                startColumn++;
+                break;        
+    }
+}
+
+void FillForward(int[,] arrayIn, int i, int j, bool isRow, int fixedIndex)
+{
+    for (; i <= j; i++)
+    {
+        if (isRow) 
+            arrayIn[fixedIndex,i] = ++step;
+        else
+            arrayIn[i,fixedIndex] = ++step;
+    }
+}
+
+void FillBack(int[,] arrayIn, int i, int j, bool isRow, int fixedIndex)
+{
+    for (; i >= j; i--)
+    {
+        if (isRow) 
+            arrayIn[fixedIndex,i] = ++step;
+        else
+            arrayIn[i,fixedIndex] = ++step;
+    }
+}
